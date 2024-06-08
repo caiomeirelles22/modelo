a8b080283b86f3580b524719e5d8c6e1dc47651e
caiomeirelles22 <dev.meirelles22@gmail.com>
2024-06-08 14:56:33 -0300

    fix

--- modelo-vereador/src/app/layout.tsx
+++ modelo-vereador/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Diarley Barreto",
+  title: "José das Couves",
   description: "Descrição detalhada sobre o candidato.",
 };
 
